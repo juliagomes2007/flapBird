52492e58e62bed31c6b008b24f748e1313684def
Julia Gomes <juliagomes2007@gmail.com>
2024-05-15 13:43:06 -0300

    Aula 2

---
+++ Assets/Scripts/GeradorDeObstaculos.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorDeObstaculos : MonoBehaviour
+{
+    //Onde gerar? Posição do gerador
+    //Quando gerar? Tempo]
+    [SerializeField]
+    private GameObject modeloObstaculo;
+
+    [SerializeField]
+    private float tempoParaGerar = 3;
+    private float cronometro;
+
+    private void Awake()
+    {
+        this.cronometro = this.tempoParaGerar;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.cronometro -= Time.deltaTime;
+        if(this.cronometro < 0)
+        {
+            GameObject.Instantiate(this.modeloObstaculo, this.transform.position, Quaternion.identity);
+            this.cronometro = tempoParaGerar;
+        }
+    }
+}
